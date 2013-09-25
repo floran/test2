f2d53bb3101b9201f851b641abbf9b7fea468a64
Oksana Schirba <krick_flora@mail.ru>
2013-09-25 11:56:33 +0400

    weqweqweqwe

---
+++ test2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Language" content="en" />
+	<meta name="GENERATOR" content="PHPEclipse 1.2.0" />
+	<title>TEST</title>
+</head>
+<body>
+<p>
+
+ewqeqerqewrwe
+dasdasdas
+sadasdasd
+</p>
+
+</body>
+</html>
